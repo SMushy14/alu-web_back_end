e70a79d1563e65be922ae5f39c6d239a54c69a36
SMushy14 <s.mushy1@alustudemt.com>
2025-10-12 22:15:55 +0200

    web.py

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-web cache and tracker
+Web cache and tracker
 """
 import requests
 import redis
@@ -10,22 +10,22 @@ store = redis.Redis()
 
 
 def count_url_access(method):
-    """Decorator counting how many times
-    a URL is accessed, and caching result for 10 seconds"""
+    """Decorator counting URL accesses and caching results"""
 
     @wraps(method)
     def wrapper(url):
-        count_key = "count:" + url
+        count_key = f"count:{url}"
+        cached_key = f"cached:{url}"
+
         store.incr(count_key)
 
-        cached_key = "cached:" + url
         cached_data = store.get(cached_key)
-
         if cached_data:
             return cached_data.decode("utf-8")
 
         html = method(url)
-        store.setex(cached_key, 10, html)
+
+        store.set(cached_key, html, ex=10)
         return html
 
     return wrapper
@@ -33,6 +33,6 @@ def count_url_access(method):
 
 @count_url_access
 def get_page(url: str) -> str:
-    """Returns HTML content of a url"""
+    """Fetch HTML content of a URL"""
     response = requests.get(url)
     return response.text
