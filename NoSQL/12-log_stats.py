c496650186ae463623c96f02424cbbe61e8548c1
SMushy14 <s.mushy1@alustudent.com>
2025-10-19 20:09:28 +0200

    find

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -9,12 +9,12 @@ def print_nginx_request_logs(nginx_collection):
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, count))
-    status_check = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}
+        req_count = len(list(nginx_collection.find({"method": method})))
+        print("\tmethod {}: {}".format(method, req_count))
+    status_checks_count = len(
+        list(nginx_collection.find({"method": "GET", "path": "/status"}))
     )
-    print("{} status check".format(status_check))
+    print("{} status check".format(status_checks_count))
 
 
 try:
